e01384204b151d0b0e28b56c707bdb18aaf2827d
Clémence <clemencefrz@gmail.com>
2024-02-25 18:50:30 +0100

    mobile nav

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+import { RiArrowDownSLine } from "react-icons/ri";
+import { FiDownload, FiSend } from "react-icons/fi";
+import { Button } from "./ui/button";
+import Socials from "./Socials";
+
 const Hero = () => {
   return (
     <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover">
@@ -8,15 +14,37 @@ const Hero = () => {
             <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
               Professeur particulier
             </div>
-            <h1 className="h1">Hello, mon nom est Clémence Fernandez</h1>
-            <p>Je donne des cours, et oui</p>
+            <h1 className="h1 mb-4">Hello, mon nom est Clémence Fernandez</h1>
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
+              Je donne des cours, et oui
+            </p>
+            {/* buttons */}
+            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
+              <Link href="/contact">
+                <Button className="gap-x-2">
+                  Contactez-moi
+                  <FiSend size={18} />
+                </Button>
+              </Link>
+              <Button variant="secondary" className="gap-x-2">
+                Télécharger mon CV
+                <FiDownload size={18} />
+              </Button>
+            </div>
+            {/* socials */}
+            <Socials
+              containerStyles="flex gap-x-6 mx-auto xl:mx-0"
+              iconStyles={
+                "text-foreground text-[22px] hover:text-primary transition-all"
+              }
+            />
           </div>
           {/* image */}
           <div>image</div>
         </div>
         {/* icon */}
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
-          {/* <RiArrowDownSLine /> */}
+          <RiArrowDownSLine className="text-3xl text-primary" />
         </div>
       </div>
     </section>
