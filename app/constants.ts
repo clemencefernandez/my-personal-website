ab90db4ec04a507790eb0bb78a304a6968cb47cf
Clémence <clemencefrz@gmail.com>
2024-02-25 18:50:35 +0100

    movile nav

---
+++ app/constants.ts
@@ -0,0 +1,2 @@
+export const URL_LINKEDIN_PROFILE =
+  "https://www.linkedin.com/in/cl%C3%A9mence-fernandez-1b5b03195/";
---
+++ components/Socials.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { URL_LINKEDIN_PROFILE } from "@/app/constants";
+import Link from "next/link";
+import { RiLinkedinFill } from "react-icons/ri";
+
+type Props = {
+  containerStyles: string;
+  iconStyles: string;
+};
+
+const icons = [
+  {
+    path: URL_LINKEDIN_PROFILE,
+    name: <RiLinkedinFill />,
+  },
+];
+
+const Socials = ({ containerStyles, iconStyles }: Props) => {
+  return (
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, index) => (
+        <a target="_blank" key={index} href={icon.path}>
+          <div className={`${iconStyles}`}>{icon.name}</div>
+        </a>
+      ))}
+    </div>
+  );
+};
+export default Socials;
