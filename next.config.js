50b65948416c15dd2cdc583377e4a37ed3444354
Clémence <clemencefrz@gmail.com>
2024-05-12 10:33:54 +0200

    resolve images'

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['images.unsplash.com']//pour avoir des images tests de unsplash
+    }
+}
 
 module.exports = nextConfig
