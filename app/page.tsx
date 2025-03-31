4a2399a773a9a15183ca5d03ca43bf8a79c742c9
clemencefrz <clemencefrz@gmail.com>
2025-03-31 17:29:34 +0200

    use ract hook form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 
 import { Button } from "@/components/ui/button";
@@ -9,45 +10,78 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
-    <>
-      <Card className="w-[550px]">
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>
-            Deploy your new project in one-click.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="firstName">{`Quel est le nom de l'attaquant ?`}</Label>
-              <div className="flex gap-2">
-                <Input
-                  id="firstName"
-                  placeholder="Format de réponse : **** ****"
-                />
-                <Button>Envoyer</Button>
-              </div>
-              <Label htmlFor="lastName">{`Quel est le nom de l'attaquant ?`}</Label>
-              <div className="flex gap-2">
-                <Input
-                  id="lastName"
-                  placeholder="Format de réponse : **** ****"
-                />
-                <Button>Envoyer</Button>
-              </div>
-            </div>
+    <Card className="w-[550px]">
+      <CardHeader>
+        <CardTitle>Première étape</CardTitle>
+        <CardDescription>
+          Voici la descriptio nde la première étape.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{`Quel est le nom de l'attaquant ?`}</FormLabel>
+                  <FormControl>
+                    <div className="flex gap-2">
+                      <Input
+                        placeholder="Format de réponse : **** ****"
+                        {...field}
+                      />
+                      <Button type="submit">Envoyer</Button>
+                    </div>
+                  </FormControl>
+                  <FormDescription>
+                    {`Plus d'informations sur cette question.`}
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </form>
-        </CardContent>
-        <CardFooter className="flex">
-          <Button className="w-full">{`Passer à l'étape suivante`}</Button>
-        </CardFooter>
-      </Card>
-    </>
+        </Form>
+      </CardContent>
+      <CardFooter className="flex">
+        <Button className="w-full">{`Passer à l'étape suivante`}</Button>
+      </CardFooter>
+    </Card>
   );
 }
